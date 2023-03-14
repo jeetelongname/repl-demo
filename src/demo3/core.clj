a43fdd1fdc907b86c16b4e850c30d294b607adbb
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-03-14 16:52:34 +0000

    feat: finish demo

--- src/demo3/core.clj
+++ src/demo3/core.clj
@@ -1,36 +1,76 @@
 (ns demo3.core
   (:require
-   [reitit.ring :as ring]
    [ring.adapter.jetty :as j]
    [clojure.pprint :as pp]
-   [hiccup.core :as h]))
+   [hiccup.core :as h]
+   [clojure.string :as str]))
 
-(defn hello-handler [request]
+(defn document
+  [& body]
+  (h/html
+   [:html
+    [:head
+     [:link {:rel "stylesheet" :href "https://unpkg.com/@picocss/pico@1.*/css/pico.min.css"}]]
+    [:body.container body]]))
+
+(defn hello-handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello Joe\n"})
+   :body #_{:clj-kondo/ignore [:deprecated-var]}
+   (h/html (document [:h1 [:bold "Hello everyone"]]))})
 
 (defn sum-handler
-  ""
   [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (h/html [:p "hello world"])})
+  (let [number (if (:query-string request)
+                 (-> request
+                     :query-string
+                     (str/split #"=")
+                     last
+                     parse-long)
+                 nil)
 
-(def router {"/hello" hello-handler
-             "/sum"   sum-handler})
+        number-str (loop [n number sum 0]
+                     (cond
+                       (nil? n) "No number provided"
+                       (= number 13) "Error"
+                       (= 0 n)  (str sum)
+                       :else (recur (dec n) (+ sum n))))
+
+        ;; number-str (if (nil? number)
+        ;;              "No number provided"
+        ;;              (/ (* number (inc number)) 2))
+        ]
+
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (h/html (document
+                    [:h1 "Sum some numbers"]
+                    [:form {:method "get" :action "/sum"}
+                     [:label {:for "number"} "Enter Number"]
+                     [:input#number {:type "text" :name "number"}]]
+                    [:p "Sum: " number-str]))}))
+
+(def router {"/hello" #'hello-handler
+             "/sum"   #'sum-handler})
 
 (defn default [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (h/html [:p "Hello from " [:strong (:uri request)]])})
+   :body (h/html
+          (document
+           [:h1 "Hello from " [:em (:uri request)]]
+           [:p "somehting"]))})
 
 (defn app
   [request]
   (pp/pprint request)
-  (let [handler (get router (:uri request) default)]
+  (let [handler (get router (:uri request) #'default)]
     (handler request)))
 
+(def start #(j/run-jetty #'app {:port 3000 :join? false}))
+
+(defn -main []
+  (start))
+
 (comment
-  (defonce server (j/run-jetty #'app {:port 3000 :join? false}))
-  (.stop server))
+  (println (+ 1 1)))
